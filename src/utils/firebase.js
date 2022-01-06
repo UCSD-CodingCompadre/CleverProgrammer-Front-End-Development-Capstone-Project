40321ccf4abc035f60c210142b38e85654fe7bcb
CodingCocho <raulujr@gmail.com>
2022-01-05 17:44:24 -0800

    Updated firebase.js now it is working and ready for deployment

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDzn4XPb8BWEmXmDS0tUTcBUmwjhuEtDwE",
-  authDomain: "capstone-a82e0.firebaseapp.com",
-  projectId: "capstone-a82e0",
-  storageBucket: "capstone-a82e0.appspot.com",
-  messagingSenderId: "209943927355",
-  appId: "1:209943927355:web:01fb0b54e807ff188c9a1e",
-  measurementId: "G-ELT9JZWPJN"
+  apiKey: "AIzaSyAmU9h9pg4OIQcGRx3BHhfoI-BCYDYBXn8",
+  authDomain: "capstone-project-8ba5e.firebaseapp.com",
+  projectId: "capstone-project-8ba5e",
+  storageBucket: "capstone-project-8ba5e.appspot.com",
+  messagingSenderId: "1056500361617",
+  appId: "1:1056500361617:web:4fe6e57608f505b9bd5230"
 };
 
 // Initialize Firebase
